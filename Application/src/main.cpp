578e210cd75ed6eb8aafb834f2789cccea092237
MIchael-Kik <kik.michael092@gmail.com>
2021-11-21 15:43:49 -0500

    updated other stuff

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -3,31 +3,6 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600, 600), "Application", sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize);
-
-    while(window.isOpen())
-    {
-
-        sf::Event evnt;
-
-        while(window.pollEvent(evnt))
-        {
-            switch(evnt.type)
-            {
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-
-                case sf::Event::Resized:
-                    std::cout << "Window width: " << evnt.size.width << ", Window Height: " << evnt.size.height << std::endl;
-                    break;
-            }
-
-
-            window.clear();
-            window.display();         
-        }
-    }
-
-    return 0;
+	std::cout << "Hello World" << std::endl;
 }
+
